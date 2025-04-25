fc4e9c5249afc1ebce85150aa132314ef572efd9
Sachibn Kumar Yadav <161759249+Sachin70115053@users.noreply.github.com>
2025-04-25 21:55:07 +0530

    Update data.js

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,11 @@
-export const USER_API_ENDPOINT ="http://13.51.175.200:5011/api/user";
-export const JOB_API_ENDPOINT = "http://13.51.175.200:5011/api/job";
-export const APPLICATION_API_ENDPOINT = "http://13.51.175.200:5011/api/application";
-export const COMPANY_API_ENDPOINT = "http://13.51.175.200:5011/api/company";
+// export const USER_API_ENDPOINT ="http://13.51.175.200:5011/api/user";
+// export const JOB_API_ENDPOINT = "http://13.51.175.200:5011/api/job";
+// export const APPLICATION_API_ENDPOINT = "http://13.51.175.200:5011/api/application";
+// export const COMPANY_API_ENDPOINT = "http://13.51.175.200:5011/api/company";
+
+
+export const USER_API_ENDPOINT = `${import.meta.env.VITE_BACKEND_API_URL}/api/user`;
+export const JOB_API_ENDPOINT = `${import.meta.env.VITE_BACKEND_API_URL}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${import.meta.env.VITE_BACKEND_API_URL}/api/application`;
+export const COMPANY_API_ENDPOINT = `${import.meta.env.VITE_BACKEND_API_URL}/company`;
+
